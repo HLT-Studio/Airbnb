d0e6d999a0ab0593fbef71e8585d88dca4a12559
Lê Văn Thắng <levanthang.2001202241@gmail.com>
2024-05-18 21:30:26 +0700

    validate register form

--- register.php
+++ register.php
@@ -34,6 +34,7 @@
                   <div class="form-outline flex-fill mb-0">
                     <input type="text" id="full_name_value" name="full_name_value" class="form-control" required>
                     <label class="form-label" for="full_name_value">Your Name</label>
+                    <span id="namevld" class="text-danger"></span>
                   </div>
                 </div>
 
@@ -42,6 +43,7 @@
                   <div class="form-outline flex-fill mb-0">
                     <input type="text" id="username" name="username" class="form-control" required>
                     <label class="form-label" for="username">User name</label>
+                    <span id="usernamevld" class="text-danger"></span>
                   </div>
                 </div>
 
@@ -50,6 +52,7 @@
                   <div class="form-outline flex-fill mb-0">
                     <input type="number" id="phone" name="phone" class="form-control" minlength="10" maxlength="10" required>
                     <label class="form-label" for="phone">Phone number</label>
+                    <span id="phonevld" class="text-danger"></span>
                   </div>
                 </div>
 
@@ -58,6 +61,7 @@
                   <div class="form-outline flex-fill mb-0">
                     <input type="email" id="email_value" name="email_value" class="form-control" required>
                     <label class="form-label" for="email_value">Your Email</label>
+                    <span id="emailvld" class="text-danger"></span>
                   </div>
                 </div>
 
@@ -66,6 +70,7 @@
                   <div class="form-outline flex-fill mb-0">
                     <input type="password" id="password_value" name="password_value" class="form-control" required>
                     <label class="form-label" for="password_value">Password</label>
+                    <span id="passvld" class="text-danger"></span>
                   </div>
                 </div>
 
@@ -74,6 +79,7 @@
                   <div class="form-outline flex-fill mb-0">
                     <input type="password" id="repassword_value" name="repassword_value" class="form-control" required>
                     <label class="form-label" for="repassword_value">Repeat your password</label>
+                    <span id="repassvld" class="text-danger"></span>
                   </div>
                 </div>
 
@@ -112,3 +118,94 @@
     </div>
   </body>
 </html>
+<script>
+    document.getElementById("namevld").hidden = true;
+    document.getElementById("usernamevld").hidden = true;
+    document.getElementById("emailvld").hidden = true;
+    document.getElementById("passvld").hidden = true;
+    document.getElementById("repassvld").hidden = true;
+    document.getElementById("phonevld").hidden = true;
+
+    document.getElementById("register_btn").addEventListener('click', (e) => {
+      document.getElementById("namevld").hidden = true;
+      document.getElementById("usernamevld").hidden = true;
+      document.getElementById("emailvld").hidden = true;
+      document.getElementById("passvld").hidden = true;
+      document.getElementById("repassvld").hidden = true;
+      document.getElementById("phonevld").hidden = true;
+        if(!validName() || !validEmail() || !validPass() || !validRepass() || !validPhone() || !validUserName())
+        {
+            e.preventDefault();
+        }
+    });
+
+function validUserName() {
+    var name = document.getElementById("username").value;
+    var regName = /^[a-zA-Z0-9]+$/;
+    if(name.length < 10)
+    {   
+        document.getElementById("usernamevld").hidden = false;
+        document.getElementById("usernamevld").innerHTML = "*Tối thiểu 10 ký tự";
+        return false;
+    }
+    if(!regName.test(name))
+    {
+        document.getElementById("usernamevld").hidden = false;
+        document.getElementById("usernamevld").innerHTML = "*Không chứa ký tự đặc biệt.";
+        return false;
+    }
+    return true;
+}
+
+function validName() {
+    var name = document.getElementById("full_name_value").value;
+    var regName = /^[a-zA-Z_ÀÁÂÃÈÉÊẾÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêếìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\ ]+$/;
+    if(!regName.test(name))
+    {
+        document.getElementById("namevld").hidden = false;
+        document.getElementById("namevld").innerHTML = "*Không chứa ký tự đặc biệt.";
+        return false;
+    }
+    return true;
+}
+function validEmail(){
+    var regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var email =  document.getElementById("email_value").value;
+    if(!regEmail.test(email)){
+        document.getElementById("emailvld").hidden = false;
+        document.getElementById("emailvld").innerHTML = "*Email không đúng định dạng.";
+        return false;
+    }
+    return true;
+}
+function validPass(){
+    var regPass = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{10,}$/;
+    var pass =  document.getElementById("password_value").value;
+    if(!regPass.test(pass)){
+        document.getElementById("passvld").hidden = false;
+        document.getElementById("passvld").innerHTML = "*Tối thiểu 10 ký tự, phải có ký tự hoa, ký tự số và ký tự đặt biệt.";
+        return false;
+    }
+    return true;
+}
+function validRepass(){
+    var repass =  document.getElementById("repassword_value").value;
+    var pass =  document.getElementById("password_value").value;
+    if(repass != pass){
+        document.getElementById("repassvld").hidden = false;
+        document.getElementById("repassvld").innerHTML = "*Mật khẩu không trùng khớp.";
+        return false;
+    }
+    return true;
+}
+function validPhone(){
+    var regPhone = /^[0-9]{10}$/;
+    var phone =  document.getElementById("phone").value;
+    if(!regPhone.test(phone)){
+        document.getElementById("phonevld").hidden = false;
+        document.getElementById("phonevld").innerHTML = "*Số điện thoại là 10 ký số.";
+        return false;
+    }
+    return true;
+}
+</script>
